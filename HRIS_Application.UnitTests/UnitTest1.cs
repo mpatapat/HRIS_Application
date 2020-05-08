3459991da7944329fffb217cbce7ed092da8a247
mpatapat <mpatapat@hotmail.com>
2020-05-09 01:54:36 +0800

    Added NUnit Library

---
+++ HRIS_Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace HRIS_Application.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
